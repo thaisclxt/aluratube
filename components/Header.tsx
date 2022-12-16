98d7b7727169998e06dd0cf8a395c6a558cfca92
thaisclxt <thais.calixto@mail.uft.edu.br>
2022-12-16 17:12:36 -0300

    integrate styled-components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,32 @@
 import config from "../config.json";
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+	img {
+		width: 80px;
+		height: 80px;
+		border-radius: 50%;
+	}
+	.user-info {
+		margin-top: 50px;
+		display: flex;
+		align-items: center;
+		width: 100%;
+		padding: 16px 32px;
+		gap: 16px;
+	}
+`;
 
 const Header = () => (
-	<section>
-		<img src={`https://github.com/${config.github}.png`} />
-		<div>
-			<h2>{config.name}</h2>
-			<p>{config.job}</p>
-		</div>
-	</section>
+	<StyledHeader>
+		<section className="user-info">
+			<img src={`https://github.com/${config.github}.png`} />
+			<div>
+				<h2>{config.name}</h2>
+				<p>{config.job}</p>
+			</div>
+		</section>
+	</StyledHeader>
 );
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
+import { CSSReset } from "../components/CSSReset";
 import Header from "../components/Header";
 import Menu from "../components/Menu";
 import Timeline from "../components/Timeline";
+import config from "../config.json";
 
 const HomePage = () => (
-	<div>
-		<Header />
-		<Menu />
-		<Timeline />
-	</div>
+	<>
+		<CSSReset />
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				flex: 1,
+			}}
+		>
+			<Header />
+			<Menu />
+			<Timeline playlists={config.playlists} />
+		</div>
+	</>
 );
 
 export default HomePage;
